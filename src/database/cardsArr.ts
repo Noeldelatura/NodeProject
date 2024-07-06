5b6849a133b9cfa71a6c84af2e3b36a0abe0401d
Noeldelatura <nellydd1425@gmail.com>
2024-07-06 23:22:39 +0300

    node

---
+++ src/database/cardsArr.ts
@@ -0,0 +1,47 @@
+const cardsArr = [
+  {
+    title: "first card",
+    subtitle: "card number the first",
+    description: "some thing",
+    phone: "053-4715267",
+    email: "nGmail1@gmail.com",
+    address: {
+      street: "231 Some St",
+      city: "Anytown",
+      country: "Israel",
+      houseNumber: 20,
+      zip: "12345",
+    },
+  },
+  {
+    title: "second card",
+    subtitle: "card number the second",
+    description: "some thing",
+    phone: "053-5439271",
+    email: "some1@gmail.com",
+    address: {
+      street: "231 Some St",
+      city: "Anytown",
+      country: "Israel",
+      houseNumber: 20,
+      zip: "12345",
+    },
+  },
+
+  {
+    title: "third card",
+    subtitle: "the card number the third",
+    description: "some thing",
+    phone: "053-5439271",
+    email: "someGmail3@gmail.com",
+    address: {
+      street: "231 Some St",
+      city: "new york city",
+      country: "USA",
+      houseNumber: 42,
+      zip: "09544",
+    },
+  },
+];
+
+export { cardsArr };
---
+++ src/database/usersArr.ts
@@ -0,0 +1,75 @@
+const users = [
+  {
+    name: {
+      first: "Admin",
+      middle: "",
+      last: "admin",
+    },
+    address: {
+      city: "SomeTown",
+      state: "",
+      country: "Germany",
+      street: "777 St",
+      zip: "11223",
+      houseNumber: 1,
+    },
+    image: {
+      alt: "user-profile",
+      url: "https://www.floranese.co.il/wp-content/uploads/IMG_1385.jpg",
+    },
+    phone: "050-1111111",
+    email: "mesAdmin.barcelona1@gmail.com",
+    password: "11223345Cc!",
+    isAdmin: true,
+    isBusiness: true,
+  },
+  {
+    name: {
+      first: "Business",
+      middle: "",
+      last: "boss",
+    },
+    address: {
+      city: "SomeTown",
+      state: "",
+      country: "Germany",
+      street: "777 St",
+      zip: "11223",
+      houseNumber: 2,
+    },
+    image: {
+      alt: "user-profile",
+      url: "https://www.floranese.co.il/wp-content/uploads/IMG_1385.jpg",
+    },
+    phone: "050-1111111",
+    email: "mesAdmin.barcelona2@gmail.com",
+    password: "11223345Cc",
+    isAdmin: false,
+    isBusiness: true,
+  },
+  {
+    name: {
+      first: "User",
+      middle: "",
+      last: "regulr",
+    },
+    address: {
+      city: "Chocolate",
+      state: "",
+      country: "Belgium",
+      street: "777 St",
+      zip: "11223",
+      houseNumber: 3,
+    },
+    image: {
+      alt: "user-profile",
+      url: "https://www.floranese.co.il/wp-content/uploads/IMG_1385.jpg",
+    },
+    phone: "050-2345678",
+    email: "mesAdmin.barcelona3@gmail.com",
+    password: "11223345Cc",
+    isAdmin: false,
+    isBusiness: false,
+  },
+];
+export { users };
